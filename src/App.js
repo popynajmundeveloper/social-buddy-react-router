4d6d0f3330f9238b84a72695894c7ab98552e3d9
popynajmundeveloper <najmun03031@gmail.com>
2020-12-06 23:14:48 +0600

    running

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Posts from './Posts/Posts';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+ .then(res=> res.json())
+.then(data=>setUsers(data));
+  },[])
   return (
     <div >
-     
+      <h3>Dynamic:{users.length}</h3>
+      <ul>
+      {users.map(user => <Posts user = {user}></Posts> )}
+      </ul>
+     <Posts></Posts>
     </div>
   );
 }
